5c0ebb3589c81f44daf2d27e4889e4eaa8d93968
김민섭 <minseop.kim@me.com>
2015-07-29 19:26:29 +0900

    버블 정렬, 병합 정렬 추가.

---
+++ Algo/Algo.Sort/BubbleSorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Sort
+{
+    /// <summary>
+    /// 버블 정렬 클래스
+    /// </summary>
+    public class BubbleSorter :Sorter
+    {
+        public BubbleSorter(int aArraySize) : base(aArraySize) { }
+        public BubbleSorter(int[] aArray) : base(aArray) { }
+
+        public override void Insert(int aIdx, int aValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algo/Algo.Sort/MergeSorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Sort
+{
+    /// <summary>
+    /// 병합 정렬 클래스
+    /// </summary>
+    public class MergeSorter :Sorter
+    {
+        public MergeSorter(int aArraySize) : base(aArraySize) { }
+        public MergeSorter(int[] aArray) : base(aArray) { }
+
+        public override void Insert(int aIdx, int aValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
